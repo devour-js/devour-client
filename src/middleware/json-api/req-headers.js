d5b059049b2aa732337f8117c10d0472163439c9
Greg Signal <greg@cardboardstraw.com>
2016-06-02 10:39:18 +1200

    Use lodash assign over Object.assign
    While this can be polyfilled as mentioned in #40, there's precedent for
    use of lodash's `assign` already in the project (in index.js). Given
    this is a pretty simple change and doesn't introduce any extra
    dependencies, I think it's worth doing this to avoid polyfilling in
    older browsers.

--- src/middleware/json-api/req-headers.js
+++ src/middleware/json-api/req-headers.js
@@ -1,10 +1,11 @@
 const isEmpty = require('lodash').isEmpty
+const assign = require('lodash').assign
 
 module.exports = {
   name: 'HEADER',
   req: (payload) => {
     if (!isEmpty(payload.jsonApi.headers)) {
-      payload.req.headers = Object.assign({}, payload.req.headers, payload.jsonApi.headers)
+      payload.req.headers = assign({}, payload.req.headers, payload.jsonApi.headers)
     }
     return payload
   }
