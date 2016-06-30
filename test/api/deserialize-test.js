7864dc599fcad3e4f7b9a23a1c928f3d01fda202
mrbabbs <giovanni.esposito@shore.com>
2016-06-30 14:48:37 +0200

    fix deserialize resources without attributes

--- test/api/deserialize-test.js
+++ test/api/deserialize-test.js
@@ -169,4 +169,74 @@ describe('deserialize', () => {
     expect(products[0].custom).to.eql(true)
     expect(products[1].custom).to.eql(true)
   })
+
+  it('should deserialize resources in data without attributes', () => {
+    jsonApi.define('product', {
+      title: '',
+      about: ''
+    })
+    let mockResponse = {
+      data: [
+        {
+          id: '1',
+          type: 'products'
+        },
+        {
+          id: '2',
+          type: 'products',
+          attributes: {
+            title: 'Another Title',
+            about: 'Another about'
+          }
+        }
+      ]
+    }
+    let products = deserialize.collection.call(jsonApi, mockResponse.data)
+    expect(products[0].title).to.be.undefined
+    expect(products[0].about).to.be.undefined
+    expect(products[1].title).to.be.eql('Another Title')
+    expect(products[1].about).to.be.eql('Another about')
+  })
+
+  it('should deserialize resources in include without attributes', () => {
+    jsonApi.define('product', {
+      title: '',
+      tags: {
+        jsonApi: 'hasMany',
+        type: 'tags'
+      }
+    })
+    jsonApi.define('tag', {
+      name: ''
+    })
+    let mockResponse = {
+      data: {
+        id: '1',
+        type: 'products',
+        attributes: {
+          title: 'hello'
+        },
+        relationships: {
+          tags: {
+            data: [
+            {id: '5', type: 'tags'},
+            {id: '6', type: 'tags'}
+            ]
+          }
+        }
+      },
+      included: [
+      {id: '5', type: 'tags'},
+      {id: '6', type: 'tags', attributes: {name: 'two'}}
+      ]
+    }
+    let product = deserialize.resource.call(jsonApi, mockResponse.data, mockResponse.included)
+    expect(product.id).to.eql('1')
+    expect(product.title).to.eql('hello')
+    expect(product.tags).to.be.an('array')
+    expect(product.tags[0].id).to.eql('5')
+    expect(product.tags[0].name).to.be.undefined
+    expect(product.tags[1].id).to.eql('6')
+    expect(product.tags[1].name).to.eql('two')
+  })
 })
