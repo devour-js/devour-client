379cc353dd5259e91bbf36a1745abfa21d9d2c3d
danwarner <dan.warner@fairwayamerica.com>
2017-08-07 15:06:06 -0700

    Add support for document top-level meta and links in deserialized response

--- test/helpers/mock-response.js
+++ test/helpers/mock-response.js
@@ -2,8 +2,13 @@ export default function (jsonApi, res = {}) {
   let mockResponse = {
     name: 'mock-response',
     req: (payload) => {
+      /*
       payload.req.adapter = function (resolve) {
         resolve(res)
+      }
+      */
+      payload.req.adapter = function () {
+        return Promise.resolve(res)
       }
       return payload
     }
