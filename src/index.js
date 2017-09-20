4b8395473b483f2430ee187d48c71b8c7f10d21b
gregpeden <greg.j.peden@gmail.com>
2017-09-20 17:37:02 -0600

    Add quotes around attempted model definition name

--- src/index.js
+++ src/index.js
@@ -344,7 +344,7 @@ class JsonApi {
   modelFor (modelName) {
     if (modelName) {
       if (!this.models[modelName]) {
-        throw new Error('API resource definition for model ' + modelName + ' not found.')
+        throw new Error('API resource definition for model \'' + modelName + '\' not found.')
       }
       return this.models[modelName] || {}
     } else {
