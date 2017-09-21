6b49abd9d1ed190608797a50153f418b616a5ca3
gregpeden <greg.j.peden@gmail.com>
2017-09-21 12:42:33 -0600

    Attempt to make error catch unit test functional, still not working

--- test/api/api-test.js
+++ test/api/api-test.js
@@ -655,8 +655,8 @@ describe('JsonApi', () => {
       jsonApi.destroy('foo', 1).then(() => done()).catch(() => done())
     })
 
-    it.skip('should throw an error while attempting to access undefined model', (done) => {
-      expect(jsonApi.findAll('derp').then(() => done()).catch(() => done())).toThrow(/API/)
+    it.skip('should throw an error while attempting to access undefined model', function (done) {
+      expect(function () { jsonApi.findAll('derp').then(done).catch(done) }).to.throwException(/API resource definition for model/)
     })
   })
 
