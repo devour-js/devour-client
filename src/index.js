59491939f858572c2350b5ff4da64b1c1f539742
nguyen ngoc son <sonnn.se@gmail.com>
2017-10-25 18:25:44 +0800

    fix error log

--- src/index.js
+++ src/index.js
@@ -348,7 +348,7 @@ class JsonApi {
 
   modelFor (modelName) {
     if (!this.models[modelName]) {
-      throw new Error('API resource definition for model "${modelName}" not found.')
+      throw new Error(`API resource definition for model "${modelName}" not found.`)
     }
 
     return this.models[modelName]
