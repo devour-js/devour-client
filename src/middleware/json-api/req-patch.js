0224927eeaad7cfd1f20f0c649766a9031476072
Tomer Shalev <shalev.tomer@gmail.com>
2019-07-19 23:09:25 +0300

    Improve coding style

--- src/middleware/json-api/req-patch.js
+++ src/middleware/json-api/req-patch.js
@@ -10,18 +10,14 @@ module.exports = {
         'Content-Type': 'application/vnd.api+json',
         'Accept': 'application/vnd.api+json'
       }
-      if (payload.req.data.constructor === Array) {
-        payload.req.data = {
-          data: serialize.collection.call(jsonApi, payload.req.model, payload.req.data),
-          meta: payload.req.meta
-        }
-      } else {
-        payload.req.data = {
-          data: serialize.resource.call(jsonApi, payload.req.model, payload.req.data),
-          meta: payload.req.meta
-        }
+      const serializer = payload.req.data.constructor === Array ? serialize.collection : serialize.resource
+
+      payload.req.data = {
+        data: serializer.call(jsonApi, payload.req.model, payload.req.data),
+        meta: payload.req.meta
       }
     }
+
     return payload
   }
 }
