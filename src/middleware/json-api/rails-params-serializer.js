7f1795ac7204585c73b6071c4ec15a530600ee0b
Davy Cardinaal <davy@jobport.nl>
2022-11-03 09:26:19 +0100

    Set paramsSerializer as an object
    The interface changed in the 1.1.3 release of axios.

--- src/middleware/json-api/rails-params-serializer.js
+++ src/middleware/json-api/rails-params-serializer.js
@@ -4,11 +4,13 @@ module.exports = {
   name: 'rails-params-serializer',
   req: (payload) => {
     if (payload.req.method === 'GET') {
-      payload.req.paramsSerializer = function (params) {
-        return Qs.stringify(params, {
-          arrayFormat: 'brackets',
-          encodeValuesOnly: true
-        })
+      payload.req.paramsSerializer = {
+        serialize: function (params) {
+          return Qs.stringify(params, {
+            arrayFormat: 'brackets',
+            encodeValuesOnly: true
+          })
+        }
       }
     }
 
